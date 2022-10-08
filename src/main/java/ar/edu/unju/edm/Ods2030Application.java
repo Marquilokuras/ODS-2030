9be64246f70d633e744b7f0196c80823b226a2c3
Marcos Quinteros <92806059+Marquilokuras@users.noreply.github.com>
2022-10-07 22:21:20 -0300

    Inicializacion del Proyecto

---
+++ src/main/java/ar/edu/unju/edm/Ods2030Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ods2030Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ods2030Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/edm/Ods2030ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ods2030ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
