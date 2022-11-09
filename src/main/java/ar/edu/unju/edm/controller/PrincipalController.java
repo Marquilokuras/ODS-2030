65d0e4fc9e6ce3e0ba2f235fba6b17c2401c0910
Marcos Quinteros <92806059+Marquilokuras@users.noreply.github.com>
2022-11-08 23:23:49 -0300

    ARREGLADO EL PROBLEMA DE LOS HTML

--- src/main/java/ar/edu/unju/edm/controller/PrincipalController.java
+++ src/main/java/ar/edu/unju/edm/controller/PrincipalController.java
@@ -5,10 +5,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PrincipalController {
-	@GetMapping({"/","/index","/home"})
-	public String getIndex(){
-		
+	@GetMapping({ "/", "/index", "/home" })
+	public String getIndex() {
+
 		return "/index";
 	}
-	
+
+	@GetMapping("/obj7")
+	public String getObj7() {
+
+		return "obj7";
+	}
+
+	@GetMapping("/obj12")
+	public String getObj12() {
+
+		return "obj12";
+	}
+
+	@GetMapping("/obj15")
+	public String getObj15() {
+
+		return "obj15";
+	}
+
+	@GetMapping("equipo")
+	public String getEquipo() {
+
+		return "equipo";
+	}
+
+	@GetMapping("/iniciativa")
+	public String getIniciativa() {
+
+		return "iniciativa";
+	}
+
+	@GetMapping("/queHacemos")
+	public String getQueHacemos() {
+
+		return "queHacemos";
+	}
+
 }
